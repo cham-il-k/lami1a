7a3af3be705a36975a6e56d6f5868c6d4e0d1ba5
cham <kazdhicham@gmal.com>
2020-11-02 10:43:51 +0100

    delete from security

--- src/config/firabase-admin.js
+++
@@ -1,7 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://liism-2a462.firebaseio.com"
-});
